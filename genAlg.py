51ef0ba9a453efb109f3b8b1f000050c3a42cd50
BSA0 <belokoniy@gmail.com>
2019-07-03 13:05:41 +0500

    Update genAlg.py

--- genAlg.py
+++ genAlg.py
@@ -4,6 +4,10 @@ import pprint as pp
 #import matplotlib.pyplot as plt
 
 
+def random_exp(d=5, ex=1):
+		return rnd.randint(-1 * d, d) / ex
+
+
 def sum_dis(x0, y0, xs, ys):
 	s = 0
 	
@@ -12,15 +16,13 @@ def sum_dis(x0, y0, xs, ys):
 	return s
 
 
-def gen_children(anims, xs, ys):
+def gen_children(anims, xs, ys, de):
 	distances = [sum_dis(i[0], i[1], xs, ys) for i in anims]
 	
 	sum_d = sum(distances)
 	
 	
 	chances = [sum_d / distances[i] for i in range(len(distances))]
-	#for i in range(len(distances)):
-	#	print(chances[i], distances[i], anims[i])
 	
 	def random_num(chances):
 		rand = rnd.randint(0, int(10000*sum(chances))) / 10000
@@ -30,17 +32,14 @@ def gen_children(anims, xs, ys):
 			i += 1
 		return i
 	
-	def random_exp():
-		return rnd.randint(-50000, 50000) / 10000
-	
 	children = [[0, 0] for i in range(len(anims)//2)]
 	
 	for i in range(len(children)):
-		children[i][0] = anims[random_num(chances)][0] + random_exp()
-		children[i][1] = anims[random_num(chances)][1] + random_exp()
+		children[i][0] = anims[random_num(chances)][0] + random_exp(ex=de)
+		children[i][1] = anims[random_num(chances)][1] + random_exp(ex=de)
 		while children in anims:
-			children[i][0] = anims[random_num(chances)][0] + random_exp()
-			children[i][1] = anims[random_num(chances)][1] + random_exp()
+			children[i][0] = anims[random_num(chances)][0] + random_exp(ex=de)
+			children[i][1] = anims[random_num(chances)][1] + random_exp(ex=de)
 	
 	return children
 
@@ -62,21 +61,18 @@ if os.path.exists('anim.txt'):
 else:
 	anims = [[rnd.random() * 10, rnd.random() * 10] for i in range(30)]
 
-# print(anims)
+de = 1
+it = 100000
 
-# for i in range(len(xs)):
-# 	plt.scatter(xs[i], ys[i])
-
-# plt.show()
-
-# for i in range(1000):
-
-for i in range(100000):
+for i in range(it):
 	anims = sorted(anims, key=lambda anim: sum_dis(anim[0], anim[1], xs, ys))
 	alive = anims[:len(anims)//2]
-	children = gen_children(anims, xs, ys)
+	if i % 10000 == 0 and i != 0:
+		de *= 10
+		print(anims[0])
+	children = gen_children(anims, xs, ys, de)
 	anims = alive + children
-print()
+	
 
 foutAnim = open("anim.txt", 'w')
 animX = [str(i[0]) for i in anims]
