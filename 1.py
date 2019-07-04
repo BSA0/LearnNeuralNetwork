b718ecabdd6dcc8c6095578780fbba6d8a37033d
BSA0 <belokoniy@gmail.com>
2019-07-04 14:07:26 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+tests_count, sensors_count = map(int, input("Enter count of tests and sensors: ").split())
+
+f = open("input.txt", 'w')
+
+print("isTrue, nums")
+
+for i in range(tests_count):
+	test = ["0"] * (sensors_count + 1)
+	line = list(map(int, input().split()))
+	print(line)
+	test[0] = str(line[0])
+	for j in line[1:]:
+		test[j] = "1"
+	f.write(" ".join(test) + "\n")
+
+f.close()
