1d2f87cc3da2ae424f09d1cd25111cab38f46a45
BSA0 <belokoniy@gmail.com>
2019-07-05 14:05:22 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,59 +1,9 @@
 import random as rnd
 import os
-import json
 
 
-neurons_count = 10
-centres_count = 10
-sensors_count = 15
-learn_count = 100000
-activate_level = 31
-sensors = [0] * sensors_count
-
-rates = []
-if os.path.exists('rates.txt'):
-	f = open('rates.txt')
-	s = f.readline()
-	while s:
-		rates.append(list(map(int, s.split())))
-		s = f.readline()
-	f.close()
-
-if not (len(rates) == neurons_count and len(rates[0]) == sensors_count):
-	rates = [[0] * sensors_count] * neurons_count
-
-
-center_decision = []
-if os.path.exists('center_decision.txt'):
-	f = open('center_decision.txt')
-	s = f.readline()
-	while s:
-		center_decision.append(list(map(int, s.split())))
-		s = f.readline()
-	f.close()
-
-if not (len(center_decision) == centres_count and len(center_decision[0]) == neurons_count):
-	center_decision = [[0] * neurons_count] * centres_count
-
-f = open("input.txt")  # Считываем тесты
-s = f.readline()
-tests = []
-
-while s:
-	line = list(s.split())
-	isTrue = int(line[0])
-	test = list(map(int, line[1:]))
-	tests.append([isTrue, test])
-	s = f.readline()
-
-test_count = len(tests)
-	
-for learn_pos in range(learn_count):  # Обучение
-	test_num = rnd.randint(0, test_count - 1)
-	sensors = tests[test_num][1]
-	numTrue = tests[test_num][0]
+def teach_analyzers(rates, sensors, numTrue, neurons_count, activate_level):
 	output = []
-	result = []
 	for i in range(neurons_count):  # i - номер анализатора
 		v = 0  # значение на нейроне
 		neurons_triggered = []
@@ -69,24 +19,76 @@ for learn_pos in range(learn_count):  # Обучение
 			for k in neurons_triggered:
 				rates[i][k] -= 1
 		output.append(1 if v >= activate_level else 0)
+	return output, rates
+
+
+neurons_count = 10
+centres_count = 10
+sensors_count = 15
+learn_count = 100000
+activate_level = 33
+sensors = [0] * sensors_count
+
+rates = []
+if os.path.exists('rates.txt'):
+	f = open('rates.txt')
+	s = f.readline()
+	while s:
+		rates.append(list(map(int, s.split())))
+		s = f.readline()
+	f.close()
+
+if not (len(rates) == neurons_count and len(rates[0]) == sensors_count):
+	rates = list()
+	for i in range(neurons_count):
+		ent = list()
+		for j in range(sensors_count):
+			ent.append(0)
+		rates.append(ent)
+
+
+center_decision = []
+if os.path.exists('center_decision.txt'):
+	f = open('center_decision.txt')
+	s = f.readline()
+	while s:
+		center_decision.append(list(map(int, s.split())))
+		s = f.readline()
+	f.close()
+
+if not (len(center_decision) == centres_count and len(center_decision[0]) == neurons_count):
+	center_decision = list()
+	for i in range(centres_count):
+		ent = list()
+		for j in range(neurons_count):
+			ent.append(0)
+		center_decision.append(ent)
+
+
+f = open("input.txt")  # Считываем тесты
+s = f.readline()
+tests = []
+
+while s:
+	line = list(s.split())
+	isTrue = int(line[0])
+	test = list(map(int, line[1:]))
+	tests.append([isTrue, test])
+	s = f.readline()
+f.close()
+
+test_count = len(tests)
 	
-	for i in range(centres_count):  # i - номер центра принятия решения
-		v = 0  # значение на центре
-		centres_triggered = []
-		for j in range(len(output)):  # j - номер анализатора и его коэф к центру
-			v += output[j] * center_decision[i][j]
-			if output[j]:
-				centres_triggered.append(j)
-		# ругаем за ошибки
-		if numTrue == i and v < activate_level:  # не узнал своего
-			for k in centres_triggered:
-				center_decision[i][k] += 1
-		elif numTrue != i and v >= activate_level:  # признал чужого
-			for k in centres_triggered:
-				center_decision[i][k] -= 1
-		result.append(1 if v >= activate_level else 0)
-	print(numTrue, result)
+for learn_pos in range(learn_count):  # Обучение
+	test_num = rnd.randint(0, test_count - 1)
+	sensors = tests[test_num][1]
+	numTrue = tests[test_num][0]
 	
+	output, rates = teach_analyzers(rates, sensors, numTrue, neurons_count, activate_level)
+	result, center_decision = teach_analyzers(center_decision, output, numTrue, centres_count, activate_level)
+	
+	if learn_pos % 10000 == 0:
+		print(learn_pos)
 
 f = open('rates.txt', 'w')
 print(*rates, sep='\n')
@@ -101,5 +103,3 @@ print(*center_decision, sep='\n')
 for rate in center_decision:
 	f.write(' '.join(map(str, rate)) + '\n')
 f.close()
-
-
