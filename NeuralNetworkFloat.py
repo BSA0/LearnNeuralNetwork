14fd985fe2b125e017d00548a18981ee7f295e58
BSA0 <belokoniy@gmail.com>
2019-07-09 12:45:24 +0500

    Update NeuralNetworkFloat.py

--- NeuralNetworkFloat.py
+++ NeuralNetworkFloat.py
@@ -1,33 +1,31 @@
 import os
 from math import e
-import numpy as np
 import random as rnd
 from time import perf_counter as pc
 
 
 def exponent(x):
-    return 1 / (1 + e ** (-1 * x))
+    return 1 / ( 1 + np.exp(-x))
 
 
 def test_analyzers(rates, sensors):
-    return exponent(np.sum(rates * sensors, axis=1))
+    return np.sum(rates * sensors, axis=1)
 
 
-def count_errors(errors, rates): # В ОБРАТНОМ ПОРЯДКЕ!!
-    pass
-
-
-layer_count = [784, 100, 25, 10]
+layer_count = [784, 150, 75, 10]
 layers = []
 learn_count = 1  # 00000
 
+print("Started load rates")
 for i in range(1, len(layer_count)):
     if os.path.exists('{}rates.txt'.format(i)):
-        layers.append(np.loadtxt('{}rates.txt'.format(i), delimiter=" ", dtype=np.uint8))
+        layers.append(np.loadtxt('{}rates.txt'.format(i), delimiter=" "))
     else:
         layers.append(np.random.random((layer_count[i], layer_count[i - 1])))
-
+print("Loaded")
 # print(*layers, sep='\n')  # debug
+
+
 print("Started load trains")
 t = pc()
 train_data = np.loadtxt("mnist_train.csv", delimiter=",", dtype=np.uint8)
@@ -39,21 +37,29 @@ for learn_step in range(learn_count):
     # Подгружаем тест
     test_num = rnd.randint(0, train_count)
     correct_ans = train_data[test_num][0]
-    in_ = train_data[test_num][1:]
     # Ведём логи
     values_ = np.empty(len(layer_count))
-    values_[0] = in_
+    values_[0] = train_data[test_num][1:] / 255
 
     # Прямой проход (-> Заполнение нейронов данными)
     for i in range(len(layers)):
-        in_ = test_analyzers(layers[i], in_)
-        values_[1] = in_
+        values_[i + 1] = exponent(test_analyzers(layers[i], values_[i]))
 
-    # Смотрим результаты и выбираем провинившихся (или нет)
+    # Смотрим результаты и считаем ошибки (<- Заполнение нейронов ошибками)
+    errors_rates = np.empty(len(layers))
+    errors_rates[-1] = np.zeros(layer_count[-1]) - values_[-1]
+    errors_rates[-1][correct_ans] += 1
+    for i in range(-1, -1 * len(layer_count), -1):
+        errors_rates[i - 1] = test_analyzers(layers[i], errors_rates[i])
+    
+    # Меняем веса
+    for i in range(len(layers)):
+        d = np.empty((layer_count[i + 1], layer_count[i]))
+        for j in range(layer_count[i + 1]):
+            d[j] = values[j] * errors_rates[j]
+        layers[i] += d
 
-    activated_neuron = np.argmax(in_)
-    if activated_neuron != correct_ans:
-        errors_ = np.empty(len(layer_count))
-        # Обратный проход (<- Высчитывание ошибок *ЫЫЫ рекурсия*)
-
-
+for i, v in enumerate(layers):
+    with open('{}rates.txt'.format(i), 'w') as f:
+        for j in v:
+            f.write(" ".join(j))
